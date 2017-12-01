3eaa2d931205a7f48e47df2c9681a6e1e653218e
Wouter Mandemaker <Wouter.Mandemaker@nice.com>
2017-12-01 10:11:45 +0100

    Initial commit.

---
+++ webApiSignalR/Hubs/Chat.cs
@@ -0,0 +1,13 @@
+﻿using System.Threading.Tasks;
+using Microsoft.AspNetCore.SignalR;
+
+namespace webApiSignalR.Hubs
+{
+	public class Chat : Hub
+	{
+		public Task Send(string message)
+		{
+			return Clients.All.InvokeAsync("Send", message);
+		}
+	}
+}
